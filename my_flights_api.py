3407e64e20c6453b3e667bf3a67fa7512b2261a6
clarice_r <claricerdallorto@gmail.com>
2023-04-10 19:32:18 +0100

    Add delete method

--- my_flights_api.py
+++ my_flights_api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flights_data import flights
-from utils import search_flights
+from utils import search_flights, get_index
 
 app = Flask(__name__)
 
@@ -18,6 +18,13 @@ def get_flights():
 @app.route('/flights/<int:fid>')
 def get_flight_by_id(fid):
     return jsonify(search_flights(fid, flights))
+
+
+@app.route('/flights/<int:fid>', methods=['DELETE'])
+def delete_flight(fid):
+    index_to_delete = get_index(fid, flights)
+    flights.pop(flights[index_to_delete])
+    return jsonify(flights)
 
 
 if __name__ == '__main__':
