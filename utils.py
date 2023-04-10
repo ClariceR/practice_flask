c7020cec0bab229b924056a5ad8d8b1bd23d0970
clarice_r <claricerdallorto@gmail.com>
2023-04-10 19:35:40 +0100

    Add get_index helper function

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def search_flights(fid, flights):
             flights_found.append(flight)
 
     return flights_found
+
+
+def get_index(fid, flights):
+    for i, flight in enumerate(flights):
+        if flight["flight_id"] == fid:
+            return i
+    return -1
